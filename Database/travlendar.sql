99f440ff1fbc804f7bd1c4065b8d6732c16b609e
Nino <ninomaryono38@gmail.com>
2017-11-15 19:49:37 +0700

    penggantian tipe data lat lng menjadi string

--- Database/travlendar.sql
+++ Database/travlendar.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     13/11/2017 17:59:39                          */
+/* Created on:     15/11/2017 19:46:37                          */
 /*==============================================================*/
 
 
@@ -16,10 +16,10 @@ create table EVENT
    EVENT_ID             int not null auto_increment,
    EMAIL                varchar(40) not null,
    EVENT_NAME           varchar(40) not null,
-   LAT_FROM             bigint not null,
-   LONG_FROM            bigint not null,
-   LAT_TO               bigint not null,
-   LONG_TO              bigint not null,
+   LAT_FROM             varchar(30) not null,
+   LONG_FROM            varchar(30) not null,
+   LAT_TO               varchar(30) not null,
+   LONG_TO              varchar(30) not null,
    ARRIVAL_TIME         datetime not null,
    EVENT_END            datetime not null,
    DEPARTURE_TIME       datetime not null,
@@ -38,6 +38,6 @@ create table USER
    primary key (EMAIL)
 );
 
-alter table EVENT add constraint FK_HAVE foreign key (EMAIL)
+alter table EVENT add constraint FK_HAS foreign key (EMAIL)
       references USER (EMAIL) on delete restrict on update restrict;
 
