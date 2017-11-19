38f0d246704c60a3c6e71b9d768a2b41b4f388d1
Nino <ninomaryono38@gmail.com>
2017-11-19 07:15:52 +0700

    penambahan nama lokasi asal dan tujuan di table event

--- Database/travlendar.sql
+++ Database/travlendar.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     15/11/2017 19:46:37                          */
+/* Created on:     19/11/2017 07:13:17                          */
 /*==============================================================*/
 
 
@@ -16,10 +16,12 @@ create table EVENT
    EVENT_ID             int not null auto_increment,
    EMAIL                varchar(40) not null,
    EVENT_NAME           varchar(40) not null,
-   LAT_FROM             varchar(30) not null,
-   LONG_FROM            varchar(30) not null,
-   LAT_TO               varchar(30) not null,
-   LONG_TO              varchar(30) not null,
+   NAME_ORIGIN          varchar(30) not null,
+   LAT_ORIGIN           varchar(30) not null,
+   LNG_ORIGIN           varchar(30) not null,
+   NAME_DESTINATION     varchar(30) not null,
+   LAT_DESTINATION      varchar(30) not null,
+   LNG_DESTINATION      varchar(30) not null,
    ARRIVAL_TIME         datetime not null,
    EVENT_END            datetime not null,
    DEPARTURE_TIME       datetime not null,
