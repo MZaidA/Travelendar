97c2f3f1cd0c05fc889c8d8a099def6ec9d33191
Nino <ninomaryono38@gmail.com>
2017-12-05 15:18:26 +0700

    penambahan preferensi dalam tabel user, dan pengubahan db menjadi 2 tabel lagi

--- Database/travlendar.sql
+++ Database/travlendar.sql
@@ -1,16 +1,10 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     05/12/2017 11:10:53                          */
+/* Created on:     05/12/2017 15:12:31                          */
 /*==============================================================*/
 
 
 drop table if exists EVENT;
-
-drop table if exists LOCATION;
-
-drop table if exists TRANSPORTATION;
-
-drop table if exists TRAVELING;
 
 drop table if exists USER;
 
@@ -20,71 +14,32 @@ drop table if exists USER;
 create table EVENT
 (
    EVENT_ID             int not null auto_increment,
-   EMAIL                varchar(40) not null,
-   TRANSPORTATION_NAME  varchar(20) not null,
+   USERNAME             varchar(30) not null,
+   EVENT_NAME           varchar(30) not null,
+   ARRIVAL_TIME         datetime not null,
+   EVENT_END            datetime not null,
+   DEPARTURE_TIME       datetime not null,
    ORIGIN               varchar(150) not null,
    DESTINATION          varchar(150) not null,
-   EVENT_NAME           varchar(30) not null,
+   TRAVEL_MODE          varchar(10) not null,
+   AVOID_TOLLS          bool,
    primary key (EVENT_ID)
 )
 auto_increment = 1;
-
-/*==============================================================*/
-/* Table: LOCATION                                              */
-/*==============================================================*/
-create table LOCATION
-(
-   LOCATION_NAME        varchar(150) not null,
-   primary key (LOCATION_NAME)
-);
-
-/*==============================================================*/
-/* Table: TRANSPORTATION                                        */
-/*==============================================================*/
-create table TRANSPORTATION
-(
-   TRANSPORTATION_NAME  varchar(20) not null,
-   TRAVEL_MODE          varchar(10) not null,
-   AVOID_TOLLS          bool,
-   primary key (TRANSPORTATION_NAME)
-);
-
-/*==============================================================*/
-/* Table: TRAVELING                                             */
-/*==============================================================*/
-create table TRAVELING
-(
-   TRANSPORTATION_NAME  varchar(20) not null,
-   ORIGIN               varchar(150) not null,
-   DESTINATION          varchar(150) not null,
-   DEPARTURE_TIME       datetime not null,
-   ARRIVAL_TIME         datetime not null,
-   EVENT_END            datetime not null,
-   primary key (TRANSPORTATION_NAME, ORIGIN, DESTINATION)
-);
 
 /*==============================================================*/
 /* Table: USER                                                  */
 /*==============================================================*/
 create table USER
 (
-   EMAIL                varchar(40) not null,
+   USERNAME             varchar(30) not null,
    PASSWORD             varchar(20) not null,
-   primary key (EMAIL)
+   HOME                 varchar(150),
+   WORKPLACE            varchar(150),
+   MAX_WALKING          int,
+   primary key (USERNAME)
 );
 
-alter table EVENT add constraint FK_HAS foreign key (EMAIL)
-      references USER (EMAIL) on delete restrict on update restrict;
-
-alter table EVENT add constraint FK_RELATIONSHIP_5 foreign key (TRANSPORTATION_NAME, ORIGIN, DESTINATION)
-      references TRAVELING (TRANSPORTATION_NAME, ORIGIN, DESTINATION) on delete restrict on update restrict;
-
-alter table TRAVELING add constraint FK_DESTINATION foreign key (DESTINATION)
-      references LOCATION (LOCATION_NAME) on delete restrict on update restrict;
-
-alter table TRAVELING add constraint FK_ORIGIN foreign key (ORIGIN)
-      references LOCATION (LOCATION_NAME) on delete restrict on update restrict;
-
-alter table TRAVELING add constraint FK_USED foreign key (TRANSPORTATION_NAME)
-      references TRANSPORTATION (TRANSPORTATION_NAME) on delete restrict on update restrict;
+alter table EVENT add constraint FK_HAS foreign key (USERNAME)
+      references USER (USERNAME) on delete restrict on update restrict;
 
