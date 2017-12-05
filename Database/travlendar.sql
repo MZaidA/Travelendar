2f89cb6b881fc8e5cad0dd4a73adedf5ea35745c
Nino <ninomaryono38@gmail.com>
2017-12-05 10:29:27 +0700

    normalisasi ke-?

--- Database/travlendar.sql
+++ Database/travlendar.sql
@@ -1,10 +1,16 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     20/11/2017 09:14:54                          */
+/* Created on:     05/12/2017 10:27:20                          */
 /*==============================================================*/
 
 
 drop table if exists EVENT;
+
+drop table if exists LOCATION;
+
+drop table if exists TRANSPORTATION;
+
+drop table if exists TRAVEL;
 
 drop table if exists USER;
 
@@ -13,21 +19,51 @@ drop table if exists USER;
 /*==============================================================*/
 create table EVENT
 (
-   EVENT_ID             int not null auto_increment,
+   EVENT_ID             int not null,
    EMAIL                varchar(40) not null,
+   TRANSPORTATION_NAME  varchar(20) not null,
+   DESTINATION          varchar(40) not null,
+   ORIGIN               varchar(40) not null,
    EVENT_NAME           varchar(40) not null,
-   NAME_ORIGIN          varchar(40) not null,
-   LAT_ORIGIN           float not null,
-   LNG_ORIGIN           float not null,
-   NAME_DESTINATION     varchar(40) not null,
-   LAT_DESTINATION      float not null,
-   LNG_DESTINATION      float not null,
+   primary key (EVENT_ID)
+);
+
+/*==============================================================*/
+/* Table: LOCATION                                              */
+/*==============================================================*/
+create table LOCATION
+(
+   LOCATION_NAME        varchar(40) not null,
+   LAT                  float not null,
+   LNG                  float not null,
+   primary key (LOCATION_NAME)
+);
+
+/*==============================================================*/
+/* Table: TRANSPORTATION                                        */
+/*==============================================================*/
+create table TRANSPORTATION
+(
+   TRANSPORTATION_NAME  varchar(20) not null,
+   TRAVEL_MODE          varchar(10) not null,
+   TRANSIT_MODE         varchar(10),
+   AVOID_TOLLS          bool,
+   primary key (TRANSPORTATION_NAME)
+);
+
+/*==============================================================*/
+/* Table: TRAVEL                                                */
+/*==============================================================*/
+create table TRAVEL
+(
+   TRANSPORTATION_NAME  varchar(20) not null,
+   DESTINATION          varchar(40) not null,
+   ORIGIN               varchar(40) not null,
+   DEPARTURE_TIME       datetime not null,
    ARRIVAL_TIME         datetime not null,
    EVENT_END            datetime not null,
-   DEPARTURE_TIME       datetime not null,
-   primary key (EVENT_ID)
-)
-auto_increment = 1;
+   primary key (TRANSPORTATION_NAME, DESTINATION, ORIGIN)
+);
 
 /*==============================================================*/
 /* Table: USER                                                  */
@@ -35,11 +71,22 @@ auto_increment = 1;
 create table USER
 (
    EMAIL                varchar(40) not null,
-   USER_NAME            varchar(40) not null,
    PASSWORD             varchar(20) not null,
    primary key (EMAIL)
 );
 
 alter table EVENT add constraint FK_HAS foreign key (EMAIL)
       references USER (EMAIL) on delete restrict on update restrict;
+
+alter table EVENT add constraint FK_RELATIONSHIP_5 foreign key (TRANSPORTATION_NAME, DESTINATION, ORIGIN)
+      references TRAVEL (TRANSPORTATION_NAME, DESTINATION, ORIGIN) on delete restrict on update restrict;
+
+alter table TRAVEL add constraint FK_DESTINATION foreign key (ORIGIN)
+      references LOCATION (LOCATION_NAME) on delete restrict on update restrict;
+
+alter table TRAVEL add constraint FK_ORIGIN foreign key (DESTINATION)
+      references LOCATION (LOCATION_NAME) on delete restrict on update restrict;
+
+alter table TRAVEL add constraint FK_USED foreign key (TRANSPORTATION_NAME)
+      references TRANSPORTATION (TRANSPORTATION_NAME) on delete restrict on update restrict;
 
